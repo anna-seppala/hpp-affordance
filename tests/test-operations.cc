66897ee935e1fcba96b22d8b0f3df3919976520e
Akseppal <seppala@laas.fr>
2016-04-06 15:39:08 +0200

    modify tests/CMakeLists.txt and add unit tests

---
+++ tests/test-operations.cc
@@ -0,0 +1,47 @@
+// Copyright (C) 2016 LAAS-CNRS
+// Author: Anna Seppala 
+//
+// This file is part of the hpp-affordance.
+//
+// hpp-affordance is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// hpp-affordance is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with hpp-affordance.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <hpp/affordance/operations.hh>
+#include <hpp/affordance/fwd.hh>
+
+#define BOOST_TEST_MODULE test-operations
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE (test_affordance)
+
+BOOST_AUTO_TEST_CASE (operations)
+{
+	hpp::affordance::SupportOperationPtr_t support (new hpp::affordance::SupportOperation(0.3));
+  hpp::affordance::LeanOperationPtr_t lean (new hpp::affordance::LeanOperation(0.1));
+
+  std::vector <hpp::affordance::OperationBasePtr_t> operations;
+  operations.push_back(support);
+  operations.push_back(lean);
+
+  const fcl::Vec3f normal1(0, 0, 1);
+
+	BOOST_CHECK_MESSAGE (support->zWorld_ == normal1, 
+		"default value for zWorld should be " << normal1);
+
+	BOOST_CHECK_MESSAGE (lean->margin_ == 0.1, 
+		"margin should match the one given when creating operation");
+
+	BOOST_CHECK_MESSAGE (operations.size () == 2, 
+		"operation vector should have size 2 after adding support and lean");
+}
+BOOST_AUTO_TEST_SUITE_END ()
