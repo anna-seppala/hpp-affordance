35e63dab7de6af0c99ee357cc579241fb1ac9313
Akseppal <seppala@laas.fr>
2016-04-26 14:38:15 +0200

    remove outdated tests

--- tests/test-create-mesh.cc
+++ tests/test-create-mesh.cc
@@ -23,8 +23,6 @@ int main ()
   fcl::Vec3f normal1(0, 0, 1);
   fcl::Vec3f normal2(0, 1, 0);
 
-  std::vector<fcl::Vec3f> vertices;
-  std::vector<fcl::Triangle> triangles;
 
   typedef fcl::BVHModel<fcl::OBBRSS> Model;
   boost::shared_ptr<Model> model (new Model ());
@@ -47,5 +45,31 @@ int main ()
 
   hpp::affordance::SemanticsDataPtr_t h = hpp::affordance::affordanceAnalysis (obj, operations);
 
+
+  std::vector<std::vector<boost::shared_ptr<Model> > > affModels;
+  for (unsigned int affIdx = 0; affIdx < h->affordances_.size (); affIdx ++) {
+    std::vector<boost::shared_ptr<Model> > modelVec;
+    affModels.push_back (modelVec);
+    // get number of affordances of specific type (lean OR support etc)
+    // this corresponds to number of objects to be created for specific aff type
+    long unsigned int len = h->affordances_[affIdx].size ();
+    for (unsigned int idx = 0; idx < len; idx++) {
+      std::vector<fcl::Vec3f> vertices;
+      std::vector<fcl::Triangle> triangles;
+      hpp::affordance::AffordancePtr_t affPtr = h->affordances_[affIdx][idx];
+      for (unsigned int triIdx = 0; triIdx <  affPtr->indices_.size (); triIdx++) {
+        vertices.push_back (model->vertices [affPtr->indices_[triIdx]]);
+        triangles.push_back (model->tri_indices [affPtr->indices_[triIdx]]);
+      }
+      
+      boost::shared_ptr<Model> model1 (new Model ());
+      // add the mesh data into the BVHModel structure
+      model1->beginModel ();
+      model1->addSubModel (vertices, triangles);
+      model1->endModel ();
+      affModels[affIdx].push_back (model1);
+    }
+  }
+  std::cout << "affordances saved to models" << std::endl;
   return 0;
 }
