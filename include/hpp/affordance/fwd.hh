a98f5098ddd8a837d3c370b4749bd72be5f24640
Akseppal <akseppal@laas.fr>
2016-04-27 14:40:46 +0200

    cosmetic changes: fix header comments

--- include/hpp/affordance/fwd.hh
+++ include/hpp/affordance/fwd.hh
@@ -2,18 +2,18 @@
 // Copyright (c) 2016 CNRS
 // Authors: Anna Seppala
 //
-// This file is part of hpp-core
-// hpp-core is free software: you can redistribute it
+// This file is part of hpp-affordance
+// hpp-affordance is free software: you can redistribute it
 // and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version
 // 3 of the License, or (at your option) any later version.
 //
-// hpp-core is distributed in the hope that it will be
+// hpp-affordance is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Lesser Public License for more details.  You should have
 // received a copy of the GNU Lesser General Public License along with
-// hpp-core  If not, see
+// hpp-affordance  If not, see
 // <http://www.gnu.org/licenses/>.
 
 #ifndef HPP_AFFORDANCE_FWD_HH
